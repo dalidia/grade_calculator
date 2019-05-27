599f9d839b57b19bb3574ef3361283a0dafe2ddd
Lidia Ataupillco <ataupill@ualberta.ca>
2019-05-27 17:42:21 -0500

    using divs for each column instead of divs for lines

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function helloWorld() {
-	alert('Hello world');
+function createAssignments() {
+	var form = document.forms["gradeCalculator"];
+	var assignDiv = document.createElement('div');
+	var assignInput = document.createElement('input');
+	assignDiv.appendChild(assignInput);
+	form.appendChild(assignDiv);
+}
+
+function validateForm() {
 
 }
+
+window.addEventListener("load", createAssignments);
