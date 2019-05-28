cae7aad439795390bf88f6f9f75cf30fd4d77e25
Lidia Ataupillco <ataupill@ualberta.ca>
2019-05-28 13:49:19 -0500

    add more assignments functionality

--- script.js
+++ script.js
@@ -1,13 +1,38 @@
 function createAssignments() {
-	var form = document.forms["gradeCalculator"];
-	var assignDiv = document.createElement('div');
-	var assignInput = document.createElement('input');
-	assignDiv.appendChild(assignInput);
-	form.appendChild(assignDiv);
+	var divList = document.getElementsByClassName('main-content')[0].children;
+	var classNames = ['assignmentName', 'percentage', 'marks', 'outOf'];
+
+	for (let i = 0; i < divList.length; i++) {
+		let assignInput = document.createElement('input');
+		assignInput.className = classNames[i];
+		divList[i].appendChild(assignInput);
+	}
+}
+
+function showAssignments() {
+	var len = 5;
+
+	for (let i = 0; i < len; i++) {
+		createAssignments();
+	}
 }
 
 function validateForm() {
 
 }
 
-window.addEventListener("load", createAssignments);
+function processForm() {
+
+}
+
+function eventHandler() {
+	//let moreAssigns = document.getElementsByClassName('submit')[1];
+	//window.addEventListener("load", showAssignments);
+	//moreAssigns.addEventListener('click', createAssignments);
+
+}
+
+window.addEventListener("load", showAssignments);
+var moreAssigns = document.getElementsByClassName('submit')[1];
+moreAssigns.addEventListener('click', createAssignments);
+//eventHandler();
