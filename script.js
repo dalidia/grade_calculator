d383e04a81f5d6eb17d46384e5e9a4940ce72765
Lidia Ataupillco <ataupill@ualberta.ca>
2019-05-28 23:36:10 -0500

    index.html and style.css finished for a desktop but a tablet or a phone, script.js in progress

--- script.js
+++ script.js
@@ -1,38 +1,69 @@
 function createAssignments() {
-	var divList = document.getElementsByClassName('main-content')[0].children;
-	var classNames = ['assignmentName', 'percentage', 'marks', 'outOf'];
+	let divList = document.getElementsByClassName('main-content')[0];
+	let classNames = ['assignmentName', 'percentage', 'marks', 'outOf'];
+	let assignDiv = document.createElement('div');
+	assignDiv.className = 'sections';
+	let assignInput;
+	let text;
 
-	for (let i = 0; i < divList.length; i++) {
-		let assignInput = document.createElement('input');
+	for (let i = 0; i < classNames.length; i++) {
+		assignInput = document.createElement('input');
 		assignInput.className = classNames[i];
-		divList[i].appendChild(assignInput);
+		assignInput.type = "text";
+		assignInput.name = classNames[i];
+		assignDiv.appendChild(assignInput);
+		text = document.createTextNode('\n');
+		assignDiv.appendChild(text);
 	}
+	divList.appendChild(assignDiv);
 }
 
 function showAssignments() {
-	var len = 5;
+	let len = 5;
 
 	for (let i = 0; i < len; i++) {
 		createAssignments();
 	}
 }
 
-function validateForm() {
+function removeAssignments() {
+	var divList = document.getElementsByClassName('main-content')[0];
+	var divLastChild = divList.lastElementChild;
+	divList.removeChild(divLastChild);
+}
+
+function validateForm(courseName, assignmentName, percentage, marks, outOf) {
 
 }
 
 function processForm() {
+  let courseName = document.getElementById('courseName');
+  courseName = courseName.value;
 
+  if (typeof courseName === 'string' || courseName instanceof String) {
+
+  }
+  alert(courseName);
+  return false;
 }
 
 function eventHandler() {
-	//let moreAssigns = document.getElementsByClassName('submit')[1];
-	//window.addEventListener("load", showAssignments);
-	//moreAssigns.addEventListener('click', createAssignments);
+	let moreAssigns = document.getElementsByClassName('submit')[1];
+	window.addEventListener("load", showAssignments);
 
+	let buttonHandler = document.getElementsByClassName('button')[1];
+	buttonHandler.addEventListener('click', (event) => {
+		if (event.target.tagName == 'INPUT') {
+			if (event.target.className == 'add') {
+				createAssignments();
+			}
+			if (event.target.className == 'remove') {
+				removeAssignments();
+			}
+		}
+	});
 }
 
-window.addEventListener("load", showAssignments);
-var moreAssigns = document.getElementsByClassName('submit')[1];
-moreAssigns.addEventListener('click', createAssignments);
-//eventHandler();
+eventHandler();
+
+
